79a0e986268a147fc0fa14516bc491d08ec20b29
Arjay Comabig <109714078+JangiGabmeister2@users.noreply.github.com>
2023-10-16 13:24:57 +1100

    created folder structure in project

---
+++ Assets/ED_GenerateStartupFolders.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class ED_GenerateStartupFolders : MonoBehaviour
+{
+    private const string parentFolderName = "Game";
+    private static readonly string parentFolderPath = "Assets/" + parentFolderName;
+
+    private const string parentFolderCreationLog = "Parent folder created at: ";
+    private const string parentFolderExistsWarning = "Parent folder already exists at: ";
+    private const string folderCreationLog = "Folder created at: ";
+    private const string folderExistsWarning = "Folder already exists at: ";
+
+    [MenuItem("Startup/Generate Startup Folders")]
+    static void CreateCommonProjectFolders()
+    {
+        if (!AssetDatabase.IsValidFolder(parentFolderPath))
+        {
+            AssetDatabase.CreateFolder("Assets", parentFolderName);
+            Debug.Log(parentFolderCreationLog + parentFolderPath);
+        }
+        else
+        {
+            Debug.LogWarning(parentFolderExistsWarning + parentFolderPath);
+        }
+
+        string[] folderNames = new string[]
+        {
+            "Animations",
+            "Audio",
+            "Documentation",
+            "Editor",
+            "Fonts",
+            "Geomat",
+            "Materials",
+            "PhysicsMaterials",
+            "Plugins",
+            "Prefabs",
+            "Resources",
+            "Scenes",
+            "Scripts",
+            "Shaders",
+            "Textures",
+            "UI"
+        };
+
+        folderNames = folderNames.OrderBy(name => name).ToArray();
+
+        foreach (string folderName in folderNames)
+        {
+            string path = parentFolderPath + "/" + folderName;
+
+            if (!AssetDatabase.IsValidFolder(path))
+            {
+                AssetDatabase.CreateFolder(parentFolderPath, folderName);
+                Debug.Log(folderCreationLog + path);
+            }
+            else
+            {
+                Debug.LogWarning(folderExistsWarning + path);
+            }
+        }
+    }
+}
