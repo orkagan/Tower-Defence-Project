0433119e9d64f729dcbd6e315294c3825a534d2e
jmcdermott-au <63567692+jmcdermott-au@users.noreply.github.com>
2023-10-16 13:52:55 +1100

    Script Test Commit
    just making sure nothing breaks

---
+++ Assets/Game/Scripts/Entity.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    private float health;
+
+    //CURRENTLY A TEST SCRIPT, IGNORE
+}
