04bb4c722d1d5154812d02ee9e93bbb5ae581634
jmcdermott-au <jmcdermott.au@gmail.com>
2023-10-21 14:57:53 +1100

    Organized cameraHandler
    done!

--- Assets/Game/Scripts/Player/CameraHandler.cs
+++ Assets/Game/Scripts/Player/CameraHandler.cs
@@ -1,39 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-
 public class CameraHandler : MonoBehaviour
 {
-    #region pseudocode
-    /*
-   tween the camera in the direction the player is moving, when holding the input keys
-    variables include: 
-    the tween speed (i.e how fast it takes to get to max)
-    distance: how far the camera moves in the given direction
-
-
-     */
-    #endregion
+    
 
     #region Variables
-    private float horizontalInput;
-    public float horizontalMultiplier;
+    public Transform cam; //This name is deceptive, it goes on pivot, not cam
 
-    private float verticalInput;
-    public float verticalMultiplier;
+    private float horizontalInput; //Input.GetAxis(horizontal)
 
-    public float leavingTime;
-    public float returningTime;
-    public float returningInputThreshold; //doesnt matter for PC
+    [Header("Multipliers")]
+    public float horizontalMultiplier; //increases the amount the camera will move east and west
 
-    private float smoothTime;
+    private float verticalInput; //Input.GetAxis(Vertical)
+    public float verticalMultiplier; //increases the amount the camera will move north and south
+
+    [Header("Leaving/Returning")]
+    public float leavingTime; //(seconds) how long does it take for the camera to move TOWARDS the desired location
+    public float returningTime; //(seconds) how long does it take for the camera to RETURN from its location
+    public float returningInputThreshold; //the input threshold for the camera acting as returning from its location 
+    //^^^ (returningInputThreshold is scuffed, if needed James will come up with a better solution)
+
+    private float smoothTime; //both of these are for Vector3.SmoothDamp
     private Vector3 velocity = Vector3.zero;
-    
-    public Transform cam;
 
-    private Vector3 toMove;
-    public float strength;
+
+    
+    private Vector3 toMove; //used in converting the horizontal and vertical inputs into horizontal and vertical camera movements
+    [Space(20f)]
+    public float strength; //how far the camera moves compared to the input
     #endregion
 
     #region Methods
@@ -46,33 +40,40 @@ public class CameraHandler : MonoBehaviour
         //Debug.DrawRay(cam.position, cam.right, Color.red);
         #endregion
 
-        //how do i figure out if it's returning or leaving???
+       
 
-
+        //creates the vector3 we'll use to move the camera, based off the player's inputs
         toMove = cam.forward * verticalInput * verticalMultiplier + cam.right * horizontalInput * horizontalMultiplier;
 
-        Vector3 newMove = Quaternion.AngleAxis(-30, cam.right) * toMove;           //TODO: this might be performance destroying, optimize by creating one vector3/ angle set on start
-        Debug.DrawRay(cam.position, newMove, Color.magenta, 5f);
-        Debug.DrawRay(cam.position, toMove, Color.white, 5f);
 
+        //Rotate the Vector3 toMove by 30 degrees on a local axis, to make it move vertically,
+        //the amount of degrees should always be equal to the Pivot object's x axis, multiplied by -1.
+        //This hardcode will cause bad things later, but I don't want to fix it. Too bad!
+        Vector3 newMove = Quaternion.AngleAxis(-30, cam.right) * toMove; //TODO: Quaternion.AngleAxis might be performance destroying, optimize by creating one vector3/ angle set on start
+        
+        
+        
+        //Debug.DrawRay(cam.position, newMove, Color.magenta, 5f);
+        //Debug.DrawRay(cam.position, toMove, Color.white, 5f);
 
+        //Calculating whether the camera should be / is returning or leaving
         if (Mathf.Abs(verticalInput) <= returningInputThreshold && Mathf.Abs(horizontalInput) <= returningInputThreshold)
         {
-            smoothTime = returningTime;
+            smoothTime = returningTime; //if under threshold, returning
         }
         else
         {
-            smoothTime = leavingTime;
+            smoothTime = leavingTime; //else is leaving
         }
 
-        //cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, newMove, lerpRate);
+        //moves the camera
         cam.transform.localPosition = Vector3.SmoothDamp(cam.transform.localPosition, newMove * strength, ref velocity, smoothTime);
 
        
     }
 
 
-    private void MyInput()
+    private void MyInput() //gathers player inputs, called every tick
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
@@ -86,7 +87,8 @@ public class CameraHandler : MonoBehaviour
     public void Update()
     {
         MyInput();
-        MoveCam();
+        
+        MoveCam(); //if optimizing in future, not sure if this needs to be run every tick or every frame.
     }
     #endregion
 }
