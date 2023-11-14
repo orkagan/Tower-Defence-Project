2ab03829d9e84eea2a90bfb9aafcba03464d6229
Jonathan Wang <arachnophobicspyder@gmail.com>
2023-11-14 16:16:39 +1100

    Disconnect Function
    Disconnecting and some debug logs

--- Assets/Game/Scripts/Netcode/NetworkUIManager.cs
+++ Assets/Game/Scripts/Netcode/NetworkUIManager.cs
@@ -10,6 +10,7 @@ public class NetworkUIManager : MonoBehaviour
     //[SerializeField] private Button _serverButton;
     [SerializeField] private Button _hostButton;
     [SerializeField] private Button _joinButton;
+    [SerializeField] private Button _disconnectButton;
 
     [SerializeField] private string defaultIP = "127.0.0.1";
     [SerializeField] private int defaultPort = 7777;
@@ -17,8 +18,12 @@ public class NetworkUIManager : MonoBehaviour
     private void Awake()
     {
         //_serverButton.onClick.AddListener(()=>{NetworkManager.Singleton.StartServer();});
-        _hostButton.onClick.AddListener(()=>{HostWithIP();});
-        _joinButton.onClick.AddListener(()=>{JoinWithIP();});
+        _hostButton.onClick.AddListener(() => { HostWithIP(); });
+        _joinButton.onClick.AddListener(() => { JoinWithIP(); });
+        _disconnectButton.onClick.AddListener(() => { Disconnect(); });
+
+        NetworkManager.Singleton.OnServerStarted += () => { _disconnectButton.interactable = true; };
+        NetworkManager.Singleton.OnClientStarted += () => { _disconnectButton.interactable = true; };
     }
     public void SetConnectionData(string ip = "127.0.0.1", string port = "7777")
     {
@@ -29,21 +34,31 @@ public class NetworkUIManager : MonoBehaviour
         }
 
         ip = string.IsNullOrEmpty(ip) ? defaultIP : ip;
-        
+        Debug.Log($"Connection Data Set as {ip}:{portNum}");
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
             ip,
             (ushort)portNum
             );
     }
-    private void HostWithIP()
+    public void HostWithIP()
     {
         SetConnectionData(_ipField.text, _portField.text);
         NetworkManager.Singleton.StartHost();
+        Debug.Log("Start Host Called");
     }
 
-    private void JoinWithIP()
+    public void JoinWithIP()
     {
         SetConnectionData(_ipField.text, _portField.text);
         NetworkManager.Singleton.StartClient();
+        Debug.Log("Start Client Called");
     }
+
+    public void Disconnect()
+    {
+        NetworkManager.Singleton.Shutdown();
+        Debug.Log("Shutdown");
+        _disconnectButton.interactable = false;
+    }
+    
 }
