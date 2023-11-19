d4e2817c7550a4fd872aca6d2d1fcb1dd92826de
JangiGabmeister <109714078+JangiGabmeister2@users.noreply.github.com>
2023-11-19 22:55:52 +1100

    Merge branch 'PlayerMovement+UI+MapBranch' into MovementNewInputSystem

---
+++ Assets/Game/Scripts/DebugLog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+    public void TestFunction(string text)
+    {
+        Debug.Log(text);
+    }
+}
--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -3,167 +3,120 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     #region Variables
-    public Player player;
-    public InputMaster controls;
-    public LayerMask groundMask;
-    public GameObject raycastHitpoint;
-    public CameraHandler camHandler;
 
-    private Plane groundPlane;
-    private Plane projectilePlane;
 
-    #region Looking
-    [Header("MouseLook")]
-    public Camera cam;
-    public Transform playerTransform;
-    public Transform rotHandler;  
-
-    private Vector2 lookInputPosition;
-    private Vector2 _playerScreenPos;
-    #endregion
-    #region Moving
-    private float accelRate; //The multiplier used on the final vector
-    [Space(20f)]
     [Header("Acceleration")]
+    private float accelRate; //The multiplier used on the final vector
+
     [Tooltip("DO NOT SET HIGHER THAN MAXSPEED OR LOWER THAN 1, GAME WILL BREAK")]
     public float acceleration;
     [Tooltip("DO NOT SET HIGHER THAN MAXSPEED OR LOWER THAN 1, GAME WILL BREAK")]
     public float deceleration;
-
+    
     private float accelAmount;
     private float decelAmount;
-
+    
     public float maxSpeed; //maximum speed
+    
+
 
     public Transform orientation; //tbh i don't even remember what this does but it's set to the capsule and it works so
     public float horizontalInput; //input system for A and D keys
     public float verticalInput; //INput system for W and S keys
-
+    
     public Vector3 moveDirection;
     public Vector3 wantedDir;
 
     public Rigidbody rb;
     public CapsuleCollider col;
+
     #endregion
-    #endregion
+
     #region Unity Methods
+
+
     private void Awake()
     {
-        controls = new InputMaster();
-
         accelAmount = (50 * acceleration) / maxSpeed;
         decelAmount = (50 * deceleration) / maxSpeed;
-    }
 
-    private void OnEnable()
-    {
-        controls.Enable();
     }
-
-    private void OnDisable()
-    {
-        controls.Disable();
-    }
-
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         orientation = GetComponent<Transform>();
         rb.freezeRotation = true;
-        groundPlane = new Plane(Vector3.up, Vector3.zero);
-        projectilePlane = new Plane(Vector3.up, rb.position);
     }
 
     private void Update()
     {
+
         MyInput();
         SpeedControl();
+
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
-        DoLookage();
-        
-        player.orientation = rotHandler.forward;
+
     }
+
     #endregion
-    #region Methods   
+
+    #region Methods
+    /// <summary>
+    /// checks horizontal and vertical input    
+    /// </summary>
     private void MyInput()
     {
-        horizontalInput = controls.Player.Movement.ReadValue<Vector2>().x;     
-        verticalInput = controls.Player.Movement.ReadValue<Vector2>().y;
-       
-        lookInputPosition = controls.Player.MousePosition.ReadValue<Vector2>();
-        _playerScreenPos = cam.WorldToScreenPoint(rb.position);        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
     }
 
-    private (bool success, Vector3 position) GetMousePosition()
-    {
-        var ray = cam.ScreenPointToRay(lookInputPosition);
-
-        if (projectilePlane.Raycast(ray, out var hitInfo))
-        {
-            Vector3 hitPoint = ray.GetPoint(hitInfo);
-            raycastHitpoint.transform.position = hitPoint;
-            return (success: true, position: hitPoint);
-        }
-        else
-        {
-            Debug.LogWarning("Aiming Raycast failed");
-            return (success: false, position: Vector3.zero);
-        }      
-    }
-
-    private void DoLookage() 
-    {
-        var (success, position) = GetMousePosition();
-        if (success)
-        {
-            position.y = playerTransform.position.y;
-            var direction = position - playerTransform.position;
-            //Debug.DrawRay(playerTransform.position, direction, Color.black);
-            //Debug.DrawRay(playerTransform.position + direction, playerTransform.up * 5f);
-            
-            //Debug.DrawRay(playerTransform.position, direction, Color.white);
-
-            rotHandler.forward = direction;
-        }
-
-        Vector2 lookDirection = lookInputPosition - _playerScreenPos;
-      
-        camHandler.PMgetter = cam.ScreenToViewportPoint(lookDirection); 
-    }
-    
     private void MovePlayer()
     {
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; 
 
         var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0)); //offsets the input vector by 45degrees, for iso 
-       
         var skewedMoveDirection = matrix.MultiplyPoint3x4(moveDirection.normalized);
 
         wantedDir = skewedMoveDirection * maxSpeed;
 
         Vector3 velocityDifference = wantedDir - rb.velocity;
+        
+        #region debug rays
+        //Debug.DrawRay( new Vector3(rb.position.x, rb.position.y + 1, rb.position.z), skewedMoveDirection.normalized, Color.white, 0.1f);
+        //Debug.DrawRay( new Vector3(rb.position.x, rb.position.y + 2, rb.position.z), wantedDir, Color.red, 0.1f);
+        //Debug.DrawRay( new Vector3(rb.position.x, rb.position.y + 3, rb.position.z), velocityDifference, Color.blue, 0.1f);
+        //Debug.DrawRay( new Vector3(rb.position.x, rb.position.y + 4, rb.position.z), rb.velocity, Color.black, 0.1f);
+        #endregion
+        
+        
 
         if (wantedDir.magnitude > 0.1f)
         {
             accelRate = accelAmount;
         }
         else if (wantedDir.magnitude < 0.1f)
-        {
-            accelRate = decelAmount;
+        { 
+        accelRate = decelAmount;
         }
 
+     
         rb.AddForce(velocityDifference * accelRate, ForceMode.Acceleration);
 
-        Vector3 singleAddVelocity = new Vector3(rb.velocity.x + (Time.fixedDeltaTime * velocityDifference.x * accelRate), rb.velocity.y, rb.velocity.z + (Time.fixedDeltaTime * velocityDifference.z * accelRate));
-        
+        Vector3 singleAddVelocity = new Vector3 (rb.velocity.x + (Time.fixedDeltaTime * velocityDifference.x * accelRate), rb.velocity.y, rb.velocity.z + (Time.fixedDeltaTime * velocityDifference.z * accelRate));
+
+        //Debug.Log("Speed added from accel in one tick " + singleAddVelocity.magnitude);
         if (singleAddVelocity.magnitude > maxSpeed)
         {
-            Debug.LogWarning("Speed added from accel is higher than max speed, tell James");
+            Debug.LogWarning("Speed added from accel is higher than max speed, CATASTROPHIC, CALL JAME");
         }
+        
+        
+       
     }
 
     /// <summary>
@@ -171,6 +124,7 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     private void SpeedControl()
     {
+
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //horizontal velocity
         if (flatVel.magnitude > maxSpeed)
         {
@@ -182,6 +136,8 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = Vector3.zero;
         }
+
     }
     #endregion 
+
 }
