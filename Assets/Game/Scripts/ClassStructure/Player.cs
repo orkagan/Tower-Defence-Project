141fb5565086925ea4d27f46c0603374f8f2c7fd
Arjay Comabig <109714078+JangiGabmeister2@users.noreply.github.com>
2023-11-20 14:44:02 +1100

    Revert "Merge branch 'PlayerMovement+UI+MapBranch' into MovementNewInputSystem"
    This reverts commit d4e2817c7550a4fd872aca6d2d1fcb1dd92826de.

--- Assets/Game/Scripts/ClassStructure/Player.cs
+++ Assets/Game/Scripts/ClassStructure/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Entities/Player", fileName = "New Player")]
 public class Player : Entity
 {
     #region Fields
@@ -11,12 +10,13 @@ public class Player : Entity
     public Tower[] towers;
     public Weapon[] weapons;
     public bool readyToBeginWave;
+    public Vector3 orientation; //this comes from PlayerMovement
     #endregion
 
     #region Methods
     public void Attack()
-    { 
-    
+    {
+        weapons[0].Attack(orientation, this.transform.position);
     }
 
     public void ReadyUp()
@@ -32,6 +32,21 @@ public class Player : Entity
     public void UpgradeTower(Tower tower)
     { 
     
+    }
+
+
+    #endregion
+
+    #region Unity Methods
+
+    public void Update()
+    {
+        Debug.DrawRay(this.transform.position, orientation);
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Attack();
+        }
     }
     #endregion
 }
